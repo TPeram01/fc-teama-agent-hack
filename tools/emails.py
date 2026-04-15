328614eb90f5322c1e2cc0c6ab4c52086f7294dd
Arben Allmeta <benallmeta@hotmail.com>
2026-04-15 10:06:20 -0500

    guardrail on email

--- tools/emails.py
+++ tools/emails.py
@@ -190,7 +190,9 @@ async def send_email_tool(
 
 
 # TODO: add function tool decorator with guardrails for email moderation
-@function_tool
+@function_tool(
+        email_prompt_injection_guardrail
+)
 async def email_read_tool(
     email_id: Annotated[
         str,
